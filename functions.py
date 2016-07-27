867b3d3a13b30920c6a71ba796217f3469a266e1
Gernot Pointner <gernot.pointner@googlemail.com>
2016-07-27 22:42:31 +0200

    Adds example for pass by object reference

--- functions.py
+++ functions.py
@@ -27,5 +27,36 @@ print("Double of 5 is " + str(double_of_five))
 double_of_three = my_function()
 print("Double of 3 is " + str(double_of_three))
 
+
+# function are first class citizens
+print("my_function: " + str(my_function))
+
 print('Docs for myfunction:')
 print(my_function.__doc__) # print the function's documentation
+
+# in general function parameters are passed by object-reference
+
+
+# this does not work with primitive types likes strings and ints, as there are immutable
+# in this case new objects are created dynamically
+
+def change_string(param_to_change):
+    """Attempts to modify the given string"""
+    param_to_change = param_to_change + "blablabla"
+
+a = "hallo"
+print('a before attempt to change it ' + a)
+change_string(a)
+print('a after attempt to change it ' + a)
+
+# dicts, list and custom objects can be changed using their mutators
+
+def change_my_val_to_dict(param_to_change):
+    """Adds another key to the given dictionary"""
+    param_to_change['key_from_method'] = 'value_from_method'
+
+b = dict()
+b['myKey'] = 'myVal'
+print('b before attempt to change it ' + str(b))
+change_my_val_to_dict(b)
+print('b after attempt to change it ' + str(b))
