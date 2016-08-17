f10e64fc36ffd3d3c0a067aa00af8ef9b0a91286
Gernot Pointner <gernot.pointner@googlemail.com>
2016-08-17 07:21:25 +0200

    Add example for class/instance variables in oop

--- oop.py
+++ oop.py
@@ -18,3 +18,24 @@ s = SimpleClass()
 print(s.my_func())
 print(s.my_attr)
 
+
+# class vs instance variables
+
+class MyClass:
+
+    # class variable shared by all instances
+    common_attribute = "Common to all instances"
+
+    def __init__(self, specific):
+        """Constructor with a parameter for a specific attribute"""
+        # instance variable
+        self.instance_attribute = specific
+
+
+first_instance = MyClass("attribute of first class")
+second_instance = MyClass("attribute of second class")
+
+print("Common attribute aka class variable of first instance: " + first_instance.common_attribute)
+print("Common attribute aka class variable of second instance: " + second_instance.common_attribute)
+print("Specific attribute aka instance variable of first instance: " + first_instance.instance_attribute)
+print("Specific attribute aka instance variable of second instance: " + second_instance.instance_attribute)
